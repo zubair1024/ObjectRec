8e7b90ae6dd55d19577e3f71458bbf4a1cb5c386
Zubair Ahmed <zubair1024@gmail.com>
2014-06-10 00:14:31 +0400

    init

---
+++ ObjectRecApp/Form1.vb
@@ -0,0 +1,16 @@
+﻿Imports Emgu.CV
+Imports Emgu.CV.Util
+Imports Emgu.CV.Structure
+
+Public Class Form1
+
+    Dim capturez As Capture = New Capture
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+
+        Dim imagez As Image(Of Bgr, Byte) = capturez.QueryFrame() 'Instead of QueryFrame, you may need to do RetrieveBgrFrame depending on the version of EmguCV you download.
+
+        PictureBox1.Image = imagez.ToBitmap()
+
+    End Sub
+End Class
