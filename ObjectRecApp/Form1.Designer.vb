cabe1b24e1b9ff77903bee7471e8c28025d3199b
Zubair Ahmed <zubair1024@gmail.com>
2014-06-10 00:57:31 +0400

    1.0

--- ObjectRecApp/Form1.Designer.vb
+++ ObjectRecApp/Form1.Designer.vb
@@ -25,31 +25,111 @@ Partial Class Form1
         Me.components = New System.ComponentModel.Container()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Column1 = New System.Windows.Forms.DataGridViewImageColumn()
+        Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'PictureBox1
         '
         Me.PictureBox1.Location = New System.Drawing.Point(12, 21)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(992, 601)
+        Me.PictureBox1.Size = New System.Drawing.Size(661, 601)
         Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.PictureBox1.TabIndex = 0
         Me.PictureBox1.TabStop = False
+        '
+        'Timer1
+        '
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column2})
+        Me.DataGridView1.Location = New System.Drawing.Point(679, 21)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(325, 556)
+        Me.DataGridView1.TabIndex = 1
+        '
+        'Column1
+        '
+        Me.Column1.HeaderText = "Image"
+        Me.Column1.ImageLayout = System.Windows.Forms.DataGridViewImageCellLayout.Stretch
+        Me.Column1.Name = "Column1"
+        Me.Column1.Width = 125
+        '
+        'Column2
+        '
+        Me.Column2.HeaderText = "Label"
+        Me.Column2.Name = "Column2"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(679, 599)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Add"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(760, 599)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Recognize"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(841, 599)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 4
+        Me.Button3.Text = "Clear"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(922, 602)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 5
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1016, 634)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.DataGridView1)
         Me.Controls.Add(Me.PictureBox1)
         Me.Name = "Form1"
         Me.Text = "Form1"
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
     Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
     Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents Column1 As System.Windows.Forms.DataGridViewImageColumn
+    Friend WithEvents Column2 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
 
 End Class
--- ObjectRecApp/Form1.vb
+++ ObjectRecApp/Form1.vb
@@ -1,16 +1,54 @@
 ﻿Imports Emgu.CV
+Imports Emgu.Util
 Imports Emgu.CV.Util
 Imports Emgu.CV.Structure
 
 Public Class Form1
-
     Dim capturez As Capture = New Capture
+    Dim imagez As Image(Of Gray, Byte)
 
     Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
 
-        Dim imagez As Image(Of Bgr, Byte) = capturez.QueryFrame() 'Instead of QueryFrame, you may need to do RetrieveBgrFrame depending on the version of EmguCV you download.
+        imagez = capturez.RetrieveGrayFrame
+        imagez._EqualizeHist() 'Makes for closer comparisons
+        PictureBox1.Image = imagez.ToBitmap
 
-        PictureBox1.Image = imagez.ToBitmap()
+    End Sub
 
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        'Datagridview has 2 columns, the first is an image column, the second is a text column.
+        'The first image column has a imagelayout of stretch.
+        DataGridView1.Rows.Add()
+        DataGridView1.Rows(DataGridView1.Rows.Count - 1).Cells(0).Value = imagez.ToBitmap
+        DataGridView1.Rows(DataGridView1.Rows.Count - 1).Height = DataGridView1.Columns(0).Width * 0.75
+        'Putting a 0.75 * width for the height keeps it at 4:3 aspect ratio, so it looks normal.
+DataGridView1.Rows(DataGridView1.Rows.Count- 1).Cells(1).Value = "Image" & DataGridView1.Rows.Count - 1
+
+    End Sub
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+
+        Dim imagelist(DataGridView1.Rows.Count - 1) As Image(Of Gray, Byte)
+        Dim labellist(DataGridView1.Rows.Count - 1) As String
+
+        For i = 0 To DataGridView1.Rows.Count - 1
+            Dim tempimage As Bitmap = DataGridView1.Rows(i).Cells(0).Value
+            imagelist(i) = New Image(Of Gray, Byte)(tempimage)
+            labellist(i) = DataGridView1.Rows(i).Cells(1).Value
+        Next
+        Dim TermCrit As MCvTermCriteria = New MCvTermCriteria(16, 0.001)
+        Dim maxdistance As Integer = 5000 'The higher the number, the difference is allowed
+        Dim EobjectRec As EigenObjectRecognizer = New EigenObjectRecognizer(imagelist, labellist, maxdistance, TermCrit)
+
+        Try
+            TextBox1.Text = EobjectRec.Recognize(imagez).Label
+        Catch ex As Exception
+            TextBox1.Text = "No!" 'This means nothing was close enough for a good match
+        End Try
+
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+
+        DataGridView1.Rows.Clear()
     End Sub
 End Class
